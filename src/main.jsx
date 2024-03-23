ba694571c5df5722facead1e5400284f3e9f1422
Michelle Jemmott <michellejemmott.dev@gmail.com>
2024-03-23 01:23:53 +0000

    Disable react dev tools

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,9 @@ import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./auth/components/AuthContext.jsx";
 import "./index.css";
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
+
+if (process.env.NODE_ENV === "production") disableReactDevTools();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
